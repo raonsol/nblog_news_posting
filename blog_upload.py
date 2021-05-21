f07f168dc72c85e5d8d9fead71e94bbfcfab64ae
raonsol <windows0903@gmail.com>
2021-05-21 16:59:57 +0900

    Make py script to execute jsx script

--- blog_upload.py
+++ blog_upload.py
@@ -1,40 +1,37 @@
+#############################################################################################
+# Script Description:
+# 1. Create head image that contains following contents by user input
+#       - Headline for news content
+#       - Name of the newspaper
+#       - Current date (applied automatically)
+# 2. Accesses to the blog and shows the editor page
+#############################################################################################
+
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import pyperclip
 import time
+import os
+import json
 
 #############################################################################################
 # Type entry:
 # [MGEN] for mgen blog, [IPF] for IPFrontiers blog
-TYPE='MGEN'
+TYPE='IPF'
 # Category entry for MGEN:
 # blockchain, smart_factory, 3dmet
 # Category entry for IPF:
 # patent=특허, design=상표디자인, copyright=저작권, ipsuit=IP분쟁소송, startup=Startup Info.
-CATEGORY="3dmet"
-WEBDRIVER_PATH = "C:\\Users\\MGEN\\chromedriver.exe"
+CATEGORY="design"
+# Fill the content below:
+NEWSPAPER_NAME="서울경제"
+HEADLINE='"우리 가수 이름 쓰지마"···영탁 막걸리 때아닌 상표 논쟁'
+
+# Path of the tools needed for script
+WEBDRIVER_PATH = os.path.expanduser('~\\chromedriver.exe')
+ESTK_PATH="C:\\Program Files (x86)\\Adobe\\Adobe Utilities - CS6\\ExtendScript Toolkit CS6\\ExtendScript Toolkit.exe"
 #############################################################################################
-
-# captcha 방지를 위한 clipboard 메소드
-def clipboard_input(user_xpath, user_input):
-    temp_user_input = pyperclip.paste()  # 사용자 클립보드를 따로 저장
-
-    pyperclip.copy(user_input)
-    driver.find_element_by_xpath(user_xpath).click()
-    ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
-
-    pyperclip.copy(temp_user_input)  # 사용자 클립보드에 저장 된 내용을 다시 가져 옴
-    time.sleep(1)
-
-def getLogin(type):
-    login_fname="./logInfo_"+type+'.txt'
-    f=open(login_fname, 'r')
-    lines=f.readlines()
-    login = {"id":lines[0],"pw":lines[1]}
-    return login
-
-login=getLogin(TYPE)
 
 blog_categ_mgen={
     'blockchain':'11',
@@ -49,9 +46,42 @@ blog_categ_ipf={
     'startup':'19',
 }
 
+# clipboard method for avoid captcha 
+def clipboardInput(user_xpath, user_input):
+    temp_user_input = pyperclip.paste()  # 사용자 클립보드를 따로 저장
+
+    pyperclip.copy(user_input)
+    driver.find_element_by_xpath(user_xpath).click()
+    ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+
+    pyperclip.copy(temp_user_input)  # 사용자 클립보드에 저장 된 내용을 다시 가져 옴
+    time.sleep(1)
+
+# get login info from the loginfo_[blog_name].txt file in same directory
+def getLogin(type):
+    login_fname="./logInfo_"+type+'.txt'
+    f=open(login_fname, 'r')
+    lines=f.readlines()
+    login = {"id":lines[0],"pw":lines[1]}
+    return login
+
+# method for creating head image
+def headImageCreate(typ, category, newspaper, headline):
+    content=dict()
+    content["type"]=typ
+    content["category"]=category
+    content["newspaper"]=newspaper
+    content["headline"]=headline
+    with open('./img_content.json', 'w', encoding='utf-8') as make_file:
+        json.dump(content, make_file, indent="\t", ensure_ascii=False)
+    os.startfile(".\head_img_generator.jsx")
+
+driver=webdriver.Chrome(WEBDRIVER_PATH)
+login=getLogin(TYPE)
+
 if TYPE=='MGEN':
     blog_categ_idx=blog_categ_mgen
-if TYPE=="IPF":
+elif TYPE=="IPF":
     blog_categ_idx=blog_categ_ipf
 else: print("Blog Type error!")
 
@@ -59,14 +89,15 @@ login_url = "https://nid.naver.com/nidlogin.login"
 blog_categ_url = ('https://blog.naver.com/PostList.nhn?blogId='
     +login["id"]+'&from=postList&categoryNo='+blog_categ_idx[CATEGORY])
 
-driver=webdriver.Chrome(WEBDRIVER_PATH)
+#create headline image
+headImageCreate(TYPE, CATEGORY,NEWSPAPER_NAME, HEADLINE)
 
 # login page
 driver.get(login_url)
 time.sleep(0.2)
-clipboard_input('//*[@id="id"]', login.get("id"))
+clipboardInput('//*[@id="id"]', login.get("id"))
 time.sleep(0.1)
-clipboard_input('//*[@id="pw"]', login.get("pw"))
+clipboardInput('//*[@id="pw"]', login.get("pw"))
 driver.find_element_by_xpath('//*[@id="log.login"]').click()
 
 # browser save page
@@ -79,3 +110,5 @@ time.sleep(0.2)
 driver.get(blog_categ_url)
 driver.find_element_by_xpath('//*[@id="post-admin"]/a[1]').click()
 time.sleep(3)
+
+# TODO:fill contents
--- head_img_generator.jsx
+++ head_img_generator.jsx
@@ -1,163 +1,164 @@
-﻿// options: IPF, MGEN
-var TEMPLATE_TYPE = "IPF";
-
-// IPF: 1=특허, 2=상표디자인, 3=저작권, 4=IP분쟁소송, 5=Startup Info.
-//MGEN: 1=BLOCKCHAIN, 2=SMART FACTORY, 3=3DMET
-var TEMPLATE_NAME=1;
-
-var NEWSPAPER_NAME ="연합뉴스";
-var HEADLINE_CONTENT = "SNS 활용 마케팅 서비스 특허출원 급증…5년간 연평균 28%↑";
-
-var DEST_MGEN="C:/Users/MGEN/Desktop/LYJ/11. Blog/Output/";
-var DEST_IPF="C:/Users/MGEN/Desktop/LYJ/00. IPF/Output/";
-var TARGET_MGEN="C:/Users/MGEN/Desktop/LYJ/11. Blog/Template/";
-var TARGET_IPF="C:/Users/MGEN/Desktop/LYJ/00. IPF/new/";
+﻿//@target illustrator
+//@include "json2.js"
 
 //return date as yy-mm-dd format
-Date.prototype.yymmdd=function(){
-    var yy=this.getFullYear().toString();
-    var mm=(this.getMonth()+1).toString();
-    var dd=this.getDate().toString();
-    return yy+"-"+(mm[1]?mm:'0'+mm[0])+"-"+(dd[1]?dd:'0'+dd[0]);
+Date.prototype.yymmdd = function() {
+    var yy = this.getFullYear().toString();
+    var mm = (this.getMonth() + 1).toString();
+    var dd = this.getDate().toString();
+    return yy + "-" + (mm[1] ? mm : '0' + mm[0]) + "-" + (dd[1] ? dd : '0' + dd[0]);
 }
 
-var tempTarget=getTarget(TEMPLATE_TYPE, TEMPLATE_NAME);
+//TODO: IDE를 통하지 않고 바로 실행시 작동안됨
+//read content from JSON
+var path_JSON="~\\Desktop\\LYJ\\11. Blog\\Automation\\nblog_news_posting\\img_content.json";
+var content = readJSON(path_JSON);
+content.date = (new Date()).yymmdd();
+
+var DEST_MGEN = "C:/Users/MGEN/Desktop/LYJ/11. Blog/Output/";
+var DEST_IPF = "C:/Users/MGEN/Desktop/LYJ/00. IPF/Output/";
+var TARGET_MGEN = "C:/Users/MGEN/Desktop/LYJ/11. Blog/Template/";
+var TARGET_IPF = "C:/Users/MGEN/Desktop/LYJ/00. IPF/new/";
+
+var tempTarget = getTarget(content.type, content.category);
 var doc = getTargetFile(tempTarget);
 
-var tempDest=getDest(TEMPLATE_TYPE);
+var tempDest = getDest(content.type);
 var outPath = getDatePath(tempDest);
 var outName = getOutputName();
-var outFile = new File(outPath+outName+".png");
+var outFile = new File(outPath + outName + ".png");
 
-var content={};
-content.newspaper=NEWSPAPER_NAME;
-content.headline=HEADLINE_CONTENT;
-content.date=(new Date()).yymmdd();
+if (doc) createImage(content);
 
-if(doc) createImage(content);
+function readJSON(file_path) {
+    var f = File(file_path);
+    f.open('r');
+    var content = f.read();
+    f.close();
 
-function createImage(content){
+    content=JSON.parse(content);
+    return content;
+}
+
+function createImage(content) {
     //select item in layer
-    var layer_content=doc.layers.getByName('Content');
+    var layer_content = doc.layers.getByName('Content');
     var item_newspaper = layer_content.pageItems.getByName('newspaper');
-    var item_date=layer_content.pageItems.getByName('date');
-    var item_headline=layer_content.pageItems.getByName('headline');
+    var item_date = layer_content.pageItems.getByName('date');
+    var item_headline = layer_content.pageItems.getByName('headline');
 
     //change content
-    item_newspaper.contents=content.newspaper;
-    item_date.contents=content.date;
-    item_headline.contents=content.headline;
+    item_newspaper.contents = content.newspaper;
+    item_date.contents = content.date;
+    item_headline.contents = content.headline;
 
     //export options
-    var opt=setOption(300, false, false);
-    var color=new RGBColor();
-    color.white=255;
+    var opt = setOption(300, false, false);
+    var color = new RGBColor();
+    color.white = 255;
     var activeAB = doc.artboards[doc.artboards.getActiveArtboardIndex()];
-    
+
     //export
-    doc.imageCapture (outFile, activeAB.artboardRect, opt);
+    doc.imageCapture(outFile, activeAB.artboardRect, opt);
     alert("File exported to" + outFile.toString());
 }
 
-function getTarget(type, content){
+function getTarget(type, content) {
     var targetPath;
     var targetName;
-    if(type=="IPF"){
-        targetPath=TARGET_IPF;
-        switch(content){
-            case 1:
-            targetName="01_특허";
-            break;
-            case 2:
-            targetName="02_상표디자인";
-            break;
-            case 3:
-            targetName="03_저작권";
-            break;
-            case 4:
-            targetName="04_IP분쟁소송";
-            break;
-            case 5:
-            targetName="05_Startup information";
-            break;
+    if (type == "IPF") {
+        targetPath = TARGET_IPF;
+        switch (content) {
+            case "patent":
+                targetName = "01_특허";
+                break;
+            case "design":
+                targetName = "02_상표디자인";
+                break;
+            case "copyright":
+                targetName = "03_저작권";
+                break;
+            case "ipsuit":
+                targetName = "04_IP분쟁소송";
+                break;
+            case "startup":
+                targetName = "05_Startup information";
+                break;
             default:
-            alert("Content type mismatch");
-            return null;
-            }
-        return targetPath+targetName+".ai/";
+                alert("Content type mismatch");
+                return null;
         }
-    else if(type=="MGEN"){
-        targetPath=TARGET_MGEN;
-        switch(content){
-            case 1:
-            targetName="01_BLOCKCHAIN";
-            break;
-            case 2:
-            targetName="02_SMART_FACTORY";
-            break;
-            case 3:
-            targetName="03_3DMET";
-            break;
+        return targetPath + targetName + ".ai/";
+    } else if (type == "MGEN") {
+        targetPath = TARGET_MGEN;
+        switch (content) {
+            case "blockchain":
+                targetName = "01_BLOCKCHAIN";
+                break;
+            case "smart_factory":
+                targetName = "02_SMART_FACTORY";
+                break;
+            case "3dmet":
+                targetName = "03_3DMET";
+                break;
             default:
-            alert("Content type mismatch");
-            return null;
-            }
-        return targetPath+targetName+".ai/";
+                alert("Content type mismatch");
+                return null;
         }
-    else{
-        alert ("Blog Type mismatch");
+        return targetPath + targetName + ".ai/";
+    } else {
+        alert("Blog Type mismatch");
         return null;
     }
 }
 
-function getDest(type){
-    if(type=="IPF") return DEST_IPF;
-    else if(type=="MGEN") return DEST_MGEN;
-    else{
-        alert ("Type mismatch");
+function getDest(type) {
+    if (type == "IPF") return DEST_IPF;
+    else if (type == "MGEN") return DEST_MGEN;
+    else {
+        alert("Type mismatch");
         return null;
     }
 }
 
-function setOption(ppi, transparency, matte){
-    var options=new ImageCaptureOptions();
-    options.resolution=ppi;
-    options.transparency=transparency;
-    options.matte=matte;
-    options.antiAliasing=true;
-    
+function setOption(ppi, transparency, matte) {
+    var options = new ImageCaptureOptions();
+    options.resolution = ppi;
+    options.transparency = transparency;
+    options.matte = matte;
+    options.antiAliasing = true;
+
     return options;
 }
 
-function getTargetFile(target_path){
-    var target=new File(target_path);
+function getTargetFile(target_path) {
+    var target = new File(target_path);
     var doc = app.open(target);
     app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
-    
-    if(doc)
+
+    if (doc)
         return doc;
-    else{
+    else {
         alert("There is no such file\n");
         return null;
-     }
+    }
 }
 
-function getDatePath(outpath){
-    var date=new Date();
-    var year=date.getFullYear().toString();
-    var month=(date.getMonth()+1).toString();
-    month=month[1]?month:'0'+month[0]
-    
-    return outpath+year+"/"+month+"/";
+function getDatePath(outpath) {
+    var date = new Date();
+    var year = date.getFullYear().toString();
+    var month = (date.getMonth() + 1).toString();
+    month = month[1] ? month : '0' + month[0]
+
+    return outpath + year + "/" + month + "/";
 }
 
-function getOutputName(){
-    var date=new Date();
-    var month=(date.getMonth()+1).toString();
-    month=month[1]?month:'0'+month[0]
-    var day=date.getDate().toString();
-    day=day[1]?day:'0'+day[0]
-    
-    return month+day;
- }
+function getOutputName() {
+    var date = new Date();
+    var month = (date.getMonth() + 1).toString();
+    month = month[1] ? month : '0' + month[0]
+    var day = date.getDate().toString();
+    day = day[1] ? day : '0' + day[0]
 
+    return month + day;
+}
